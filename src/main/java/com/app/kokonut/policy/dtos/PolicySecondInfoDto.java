e967abe3537d22fa8a51297d4099caa42d90b2f8
우디 <woody@kokonut.me>
2023-05-02 16:19:54 +0900

    데이터값생성

--- src/main/java/com/app/kokonut/policy/dtos/PolicySecondInfoDto.java
+++ src/main/java/com/app/kokonut/policy/dtos/PolicySecondInfoDto.java
@@ -15,14 +15,14 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class PolicySecondInfoDto {
 
-    private Integer piInternetChose; // 인터넷접속로그 여부 0: 미선택, 1: 선택
+    private Boolean piInternetChose; // 인터넷접속로그 여부 0: 미선택, 1: 선택
 
-    private Integer piContractChose; // 계약또는청약철회 여부 0: 미선택, 1: 선택
+    private Boolean piContractChose; // 계약또는청약철회 여부 0: 미선택, 1: 선택
 
-    private Integer piPayChose; // 대금결제 및 재화 여부 0: 미선택, 1: 선택
+    private Boolean piPayChose; // 대금결제 및 재화 여부 0: 미선택, 1: 선택
 
-    private Integer piConsumerChose; // 소피자의 불만 또는 분쟁처리 여부: 0: 미선택, 1: 선택
+    private Boolean piConsumerChose; // 소피자의 불만 또는 분쟁처리 여부: 0: 미선택, 1: 선택
 
-    private Integer piAdvertisementChose; // 표시광고 0: 미선택, 1: 선택
+    private Boolean piAdvertisementChose; // 표시광고 0: 미선택, 1: 선택
 
 }
