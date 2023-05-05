19c9a7885ae35920c63e9d38a9c35184f91ddae2
서트 <cert@kokonut.me>
2023-05-05 05:47:18 +0000

    Edited start.sh

--- script/start.sh
+++ script/start.sh
@@ -2,13 +2,13 @@
 
 # Stop existing process
 echo "Stopping existing process..."
-pkill -f kokonut-0.0.1-SNAPSHOT.jar
+pkill -f kokonut*.jar
 
 # 10초간 대기합니다.
 sleep 10
 
-source ~/.zshrc
+source /root/.zshrc
 
 # 새로운 프로세스를 시작합니다.
 echo "Starting new process..."
-nohup java -jar *.jar &
+nohup java -jar kokonut-0.0.1-SNAPSHOT.jar &
