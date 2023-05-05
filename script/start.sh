3992e05068c9a9d9eab60025047f94f3dd0054f0
서트 <cert@kokonut.me>
2023-05-05 05:04:32 +0000

    Edited start.sh

--- script/start.sh
+++ script/start.sh
@@ -3,7 +3,13 @@
 # Change directory to the location of the JAR file
 cd /root/kokonut_backend/
 
-# Start the application
+# 프로세스를 죽입니다.
+pkill -f kokonut*.jar
+
+# 10초간 대기합니다.
+sleep 10
+
+# 새로운 프로세스를 시작합니다.
 source ~/.zshrc
 nohup java -jar *.jar &
 
