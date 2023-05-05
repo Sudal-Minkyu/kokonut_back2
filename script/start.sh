19f28a99fa54373902a5b8d84cd0927c8ce3bd36
서트 <cert@kokonut.me>
2023-05-05 06:11:39 +0000

    Edited start.sh

--- script/start.sh
+++ script/start.sh
@@ -2,15 +2,21 @@
 
 cd /root/kokonut_backend/
 
-# Stop existing process
-echo "Stopping existing process..."
-pkill -f kokonut*.jar
+
+# 프로세스 종료
+PID=$(pgrep -f kokonut*.jar)
+kill $PID
 
 # 10초간 대기합니다.
 sleep 10
 
+# 파일 삭제
+rm *.jar
+
+# 새로운 파일 복사
+cp /opt/codedeploy-agent/deployment-root/$DEPLOYMENT_GROUP_ID/$DEPLOYMENT_ID/deployment-archive/kokonut*.jar /root/kokonut_backend/kokonut*.jar
+
 source /root/.zshrc
 
-# 새로운 프로세스를 시작합니다.
-echo "Starting new process..."
-nohup java -jar kokonut-0.0.1-SNAPSHOT.jar &
+# 새로운 프로세스 시작
+nohup java -jar kokonut-0.0.1-SNAPSHOT.jar > /dev/null 2>&1 &
