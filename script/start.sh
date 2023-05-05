becd1855878ee6ecb97da60144c83972969eea42
서트 <cert@kokonut.me>
2023-05-05 05:15:39 +0000

    Edited start.sh

--- script/start.sh
+++ script/start.sh
@@ -1,41 +1,23 @@
 #!/bin/bash
 
-# Change directory to the location of the JAR file
-cd /root/kokonut_backend/
+# Stop existing process
+echo "Stopping existing process..."
+PID=$(ps -ef | grep kokonut-0.0.1-SNAPSHOT.jar | grep -v grep | awk '{print $2}')
+if [ -n "$PID" ]; then
+    echo "Killing process $PID"
+    kill -9 $PID
+fi
 
-# 프로세스를 죽입니다.
-pkill -f kokonut*.jar
+# Remove existing file
+echo "Removing existing file..."
+rm /root/kokonut_backend/*.jar
+
 
 # 10초간 대기합니다.
 sleep 10
 
-# 새로운 프로세스를 시작합니다.
 source ~/.zshrc
+
+# 새로운 프로세스를 시작합니다.
+echo "Starting new process..."
 nohup java -jar *.jar &
-
-
-
-#!/bin/bash
-#BUILD_JAR=$(ls /data/apps/tomcat8.5/webapps/build/*.jar)
-#JAR_NAME=$(basename $BUILD_JAR)
-#echo "> build 파일명: $JAR_NAME" >> /data/apps/tomcat8.5/webapps/deploy.log
-
-#echo "> build 파일 복사" >> /data/apps/tomcat8.5/webapps/deploy.log
-#DEPLOY_PATH=/data/apps/tomcat8.5/webapps/
-#cp $BUILD_JAR $DEPLOY_PATH
-
-#echo "> 현재 실행중인 애플리케이션 pid 확인" >> /data/apps/tomcat8.5/webapps/deploy.log
-#CURRENT_PID=$(pgrep -f $JAR_NAME)
-
-#if [ -z $CURRENT_PID ]
-#then
-#  echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다." >> /data/apps/tomcat8.5/webapps/deploy.log
-#else
-#  echo "> kill -15 $CURRENT_PID"
-#  kill -15 $CURRENT_PID
-#  sleep 5
-#fi
-
-#DEPLOY_JAR=$DEPLOY_PATH$JAR_NAME
-#echo "> DEPLOY_JAR 배포"    >> /data/apps/tomcat8.5/webapps/deploy.log
-#nohup java -jar $DEPLOY_JAR >> /data/apps/tomcat8.5/webapps/deploy.log 2>/data/apps/tomcat8.5/webapps/deploy_err.log &
