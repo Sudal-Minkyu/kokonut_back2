af8e921ce9e3997aff5edb1bef44edad597904ab
서트 <cert@kokonut.me>
2023-05-08 01:47:14 +0000

    Edited start.sh

--- script/start.sh
+++ script/start.sh
@@ -11,14 +11,14 @@ kill $PID
 sleep 10
 
 # 파일 삭제
-sudo rm *.jar
+rm *.jar
 
 # 새로운 파일 복사
-sudo cp /opt/codedeploy-agent/deployment-root/$DEPLOYMENT_GROUP_ID/$DEPLOYMENT_ID/deployment-archive/kokonut*.jar /root/kokonut_backend/kokonut*.jar
+cp /opt/codedeploy-agent/deployment-root/$DEPLOYMENT_GROUP_ID/$DEPLOYMENT_ID/deployment-archive/kokonut*.jar /root/kokonut_backend/kokonut*.jar
 
-sudo source ~/.zshrc
+source ~/.zshrc
 
 # 새로운 프로세스 시작
-sudo nohup java -jar kokonut-0.0.1-SNAPSHOT.jar 1>vite.stdout 2>vite.stderr &
+nohup java -jar kokonut-0.0.1-SNAPSHOT.jar 1>vite.stdout 2>vite.stderr &
 
 exit
