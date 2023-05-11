866a1393d394697cad87206f893b30319ba0c876
서트 <cert@kokonut.me>
2023-05-11 13:06:00 +0000

    Edited KokonutApplication.java

--- src/main/java/com/app/KokonutApplication.java
+++ src/main/java/com/app/KokonutApplication.java
@@ -8,23 +8,23 @@ public class KokonutApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(KokonutApplication.class, args);
 	}
-}
 
 // CORS 정책을 적용하기 위해 WebMvcConfigurer 인터페이스를 구현하고 addCorsMappings() 메소드를 구현
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration
-public class WebConfig implements WebMvcConfigurer {
+    @Configuration
+    public static class WebConfig implements WebMvcConfigurer {
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-            .allowedOrigins("*")
-            .allowedMethods("GET", "POST")
-            .allowedHeaders("*")
-            .allowCredentials(true)
-            .maxAge(3600);
+        @Override
+        public void addCorsMappings(CorsRegistry registry) {
+            registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(3600);
+        }
     }
 }
