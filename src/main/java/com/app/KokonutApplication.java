677cb3ee7e0ac6a1fd2cdfc13a9937e13c8f35cf
서트 <cert@kokonut.me>
2023-05-11 23:04:24 +0000

    Edited KokonutApplication.java

--- src/main/java/com/app/KokonutApplication.java
+++ src/main/java/com/app/KokonutApplication.java
@@ -4,29 +4,45 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class KokonutApplication {
-	public static void main(String[] args) {
-		SpringApplication.run(KokonutApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(KokonutApplication.class, args);
+    }
 
-
-
-// CORS 정책을 적용하기 위해 WebMvcConfigurer 인터페이스를 구현하고 addCorsMappings() 메소드를 구현
+    // CORS 정책을 적용하기 위해 WebMvcConfigurer 인터페이스를 구현하고 addCorsMappings() 메소드를 구현
     @Configuration
     public static class WebConfig implements WebMvcConfigurer {
 
         @Override
         public void addCorsMappings(CorsRegistry registry) {
             registry.addMapping("/**")
-                .allowedOrigins("*")
-                .allowedMethods("GET", "POST")
-                .allowedHeaders("*")
-                .allowCredentials(true)
-                .maxAge(3600);
+                    .allowedOrigins("*")
+                    .allowedMethods("GET", "POST")
+                    .allowedHeaders("*")
+                    .allowCredentials(true)
+                    .maxAge(3600);
+        }
+    }
+
+    @EnableWebSecurity
+    public static class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http
+                .cors().and()
+                .authorizeRequests()
+                    .antMatchers("/", "/**").permitAll()
+                    .anyRequest().authenticated()
+                    .and()
+                .httpBasic();
         }
     }
 }
