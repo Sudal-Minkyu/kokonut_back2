3fe47b928f5946c8eddd688455f60067f957db1c
서트 <cert@kokonut.me>
2023-05-12 04:00:39 +0000

    Edited KokonutApplication.java

--- src/main/java/com/app/KokonutApplication.java
+++ src/main/java/com/app/KokonutApplication.java
@@ -3,42 +3,10 @@ package com.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.server.SecurityWebFilterChain;
-import org.springframework.security.config.Customizer;
-import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.web.server.SecurityWebFilterChain;
-
-
-
 @SpringBootApplication
 public class KokonutApplication {
     public static void main(String[] args) {
         SpringApplication.run(KokonutApplication.class, args);
     }
-
-    // CORS 정책을 적용하기 위해 WebMvcConfigurer 인터페이스를 구현하고 addCorsMappings() 메소드를 구현
-@EnableWebSecurity
-public class SecurityConfig {
-
-    @Bean
-    public WebSecurityCustomizer webSecurityCustomizer() {
-        return (web) -> web.ignoring().antMatchers("/**");
-    }
-
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-                .authorizeRequests()
-                .antMatchers("/**").permitAll()
-                .anyRequest().authenticated();
-        return http.build();
-    }
-
-}
  
 }
