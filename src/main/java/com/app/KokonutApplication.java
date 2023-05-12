79844fc4074a07c452b8e7e9dd000f10e0838984
서트 <cert@kokonut.me>
2023-05-12 01:20:49 +0000

    Edited KokonutApplication.java

--- src/main/java/com/app/KokonutApplication.java
+++ src/main/java/com/app/KokonutApplication.java
@@ -3,12 +3,17 @@ package com.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+
 
 @SpringBootApplication
 public class KokonutApplication {
@@ -17,32 +22,23 @@ public class KokonutApplication {
     }
 
     // CORS 정책을 적용하기 위해 WebMvcConfigurer 인터페이스를 구현하고 addCorsMappings() 메소드를 구현
-    @Configuration
-    public static class WebConfig implements WebMvcConfigurer {
+@EnableWebSecurity
+public class SecurityConfig {
 
-        @Override
-        public void addCorsMappings(CorsRegistry registry) {
-            registry.addMapping("/**")
-                    .allowedOrigins("*")
-                    .allowedMethods("GET", "POST")
-                    .allowedHeaders("*")
-                    .allowCredentials(true)
-                    .maxAge(3600);
-        }
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring().antMatchers("/**");
     }
 
-    @EnableWebSecurity
-    public static class SecurityConfig extends WebSecurityConfigurerAdapter {
-
-        @Override
-        protected void configure(HttpSecurity http) throws Exception {
-            http
-                .cors().and()
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
                 .authorizeRequests()
-                    .antMatchers("/", "/**").permitAll()
-                    .anyRequest().authenticated()
-                    .and()
-                .httpBasic();
-        }
+                .antMatchers("/**").permitAll()
+                .anyRequest().authenticated();
+        return http.build();
     }
+
+}
+ 
 }
