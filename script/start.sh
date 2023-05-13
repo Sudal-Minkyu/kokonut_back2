e0c31444d881e27098850112b8798335dcd5e1c4
서트 <cert@kokonut.me>
2023-05-13 12:00:04 +0000

    Edited start.sh

--- script/start.sh
+++ script/start.sh
@@ -2,24 +2,23 @@
 
 cd /root/kokonut_backend/
 
-ps -ef | grep java|awk '{print $2}'
 # 프로세스 종료
+kill -9 $(ps -ef | grep java | awk '{print $2}')
 
-kill -9 ps -ef | grep java|awk '{print $2}'
+export KOKONUT_JWT_SECRET=$(aws secretsmanager get-secret-value --secret-id KOKONUT_JWT_SECRET --query SecretString --output text)
+
 
 # 10초간 대기합니다.
-sleep 15
+sleep 10
 
 # 파일 삭제
-#rm *.jar
-
+#rm /root/kokonut_backend/kokonut*.jar
 # 새로운 파일 복사
 cp /opt/codedeploy-agent/deployment-root/$DEPLOYMENT_GROUP_ID/$DEPLOYMENT_ID/deployment-archive/kokonut*.jar /root/kokonut_backend/kokonut*.jar
 
-source /root/kokonut_backend/.zshrc
-
+# source /root/kokonut_backend/.zshrc
 # 새로운 프로세스 시작
-nohup java -jar kokonut-0.0.1-SNAPSHOT.jar 1>vite.stdout 2>vite.stderr &
+
+nohup java -jar /root/kokonut_backend/kokonut*.jar > /root/kokonut_backend/vite.log 2>&1 &
 
 exit
-
