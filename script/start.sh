7335b5b53c4d65aba2af3967f75aa4836f7fe731
서트 <cert@kokonut.me>
2023-05-13 05:14:27 +0000

    Edited start.sh

--- script/start.sh
+++ script/start.sh
@@ -16,7 +16,7 @@ sleep 10
 # 새로운 파일 복사
 cp /opt/codedeploy-agent/deployment-root/$DEPLOYMENT_GROUP_ID/$DEPLOYMENT_ID/deployment-archive/kokonut*.jar /root/kokonut_backend/kokonut*.jar
 
-source ~/.zshrc
+source /root/kokonut_backend/.zshrc
 
 # 새로운 프로세스 시작
 nohup java -jar kokonut-0.0.1-SNAPSHOT.jar 1>vite.stdout 2>vite.stderr &
