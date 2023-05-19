fb1840073ff37d661ccdf47c896508501adf56c0
서트 <cert@kokonut.me>
2023-05-19 06:25:22 +0000

    Edited KokonutApplication.java

--- src/main/java/com/app/KokonutApplication.java
+++ src/main/java/com/app/KokonutApplication.java
@@ -3,6 +3,10 @@ package com.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 @SpringBootApplication
 public class KokonutApplication {
     public static void main(String[] args) {
@@ -18,5 +22,4 @@ public class KokonutApplication {
             }
         };
     }
- 
 }
