77ef595bf65d58363928c4d43e31b28121842880
서트 <cert@kokonut.me>
2023-05-19 06:46:21 +0000

    Edited KokonutApplication.java

--- src/main/java/com/app/KokonutApplication.java
+++ src/main/java/com/app/KokonutApplication.java
@@ -8,7 +8,7 @@ import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class KokonutApplication {
+public class KokonutApplication implements WebMvcConfigurer {
     public static void main(String[] args) {
         SpringApplication.run(KokonutApplication.class, args);
     }
