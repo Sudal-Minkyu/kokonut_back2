6232133a080f4d7346423021d4d557788c44feaf
서트 <cert@kokonut.me>
2023-05-19 06:12:52 +0000

    Edited KokonutApplication.java

--- src/main/java/com/app/KokonutApplication.java
+++ src/main/java/com/app/KokonutApplication.java
@@ -8,5 +8,15 @@ public class KokonutApplication {
     public static void main(String[] args) {
         SpringApplication.run(KokonutApplication.class, args);
     }
+    
+    @Bean
+    public WebMvcConfigurer webMvcConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addViewControllers(ViewControllerRegistry registry) {
+                registry.addViewController("/swagger").setViewName("forward:/swagger-ui.html");
+            }
+        }
+    }
  
 }
