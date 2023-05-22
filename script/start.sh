ca91576b0b1b4d81149f363adfd8b1879119b53e
서트 <cert@kokonut.me>
2023-05-22 23:30:07 +0000

    Edited start.sh

--- script/start.sh
+++ script/start.sh
@@ -3,10 +3,8 @@ sudo su -
 cd /root/kokonut_backend/
 
 # 프로세스 종료
-ps aux | grep java | grep -v grep | awk '{print $2}' | xargs kill -9
-
-
-source ~/.zshrc
+# ps aux | grep java | grep -v grep | awk '{print $2}' | xargs kill -9
+ps -ef | grep java | grep -v sonarqube | grep -v grep | awk '{print $2}' | xargs kill
 
 # 10초간 대기합니다.
 sleep 10
@@ -16,10 +14,11 @@ sleep 10
 # 새로운 파일 복사
 cp /opt/codedeploy-agent/deployment-root/$DEPLOYMENT_GROUP_ID/$DEPLOYMENT_ID/deployment-archive/kokonut*.jar /root/kokonut_backend/kokonut*.jar
 
-# 새로운 프로세스 시작
+source ~/.zshrc
 
+# 새로운 프로세스 시작
 mkdir /root/kokonut_backend/logs
+
 nohup java -jar -Dserver.port=8050 kokonut-0.0.1-SNAPSHOT.jar > /root/kokonut_backend/logs/$(date +%Y-%m-%d).log 2>&1 &
 
 exit
-
