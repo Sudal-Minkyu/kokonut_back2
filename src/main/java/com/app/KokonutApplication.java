74d810f9d996d30da327d1577d6884e25ef86e24
서트 <cert@kokonut.me>
2023-05-23 03:14:39 +0000

    Edited KokonutApplication.java

--- src/main/java/com/app/KokonutApplication.java
+++ src/main/java/com/app/KokonutApplication.java
@@ -3,10 +3,19 @@ package com.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
 @SpringBootApplication
-public class KokonutApplication {
+public class KokonutApplication implements WebMvcConfigurer {
     public static void main(String[] args) {
         SpringApplication.run(KokonutApplication.class, args);
+    }
+    
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/swagger").setViewName("forward:/swagger-ui/index.html");
     }
 
 }
